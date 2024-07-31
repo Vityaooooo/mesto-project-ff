017b9051a741f37d0c3f1f26ef6f6508cd7fd3e8
Ilyasov Viktor <ilasovviktor2666@gmail.com>
2024-07-31 14:41:37 +0300

    ready project

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placeItems = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteCardFunc) {
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+		
+	cardElement.querySelector('.card__image').src = cardData.link;
+	cardElement.querySelector('.card__title').textContent = cardData.name;
+	cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCardFunc);
+
+	return cardElement;
+};
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+  const card = evt.target.closest('.card');
+  card.remove();
+};
 
 // @todo: Вывести карточки на страницу
+function addCards() {
+	initialCards.forEach(element => {
+		placeItems.append(createCard(element, deleteCard));
+	});
+};
+
+addCards();
